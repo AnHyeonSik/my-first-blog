5bfec77f7c93168f59356c901343c65fba4f183d
AnHyeonSik <ok168@naver.com>
2016-08-30 00:39:15 +0900

    Added views to create/edit blog post inside the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth import views
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('news.views',
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', login, name='login'),
-    url(r'^accounts/logout/$', logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 )
